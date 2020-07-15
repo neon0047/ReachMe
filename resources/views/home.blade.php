3595521210d57c34818a48bac4e533d0df709e2e
neon0047 <neon4744@gmail.com>
2020-07-15 17:12:33 +0530

    version 1.01

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" style="color: #38c172">
+    <div class="row">
+        <div class="col-3 pl-5">
+            <img src="/images/hacker.jpg" class="rounded-circle " style="max-height: 150px">
+        </div>
+        <div class="col-9 pt-4">
+            <div><h1>ReachMe</h1></div>
+                <div class="d-flex" >
+                    <div class="pr-3"><strong>Post</strong> 24</div>
+                    <div class="pr-3"><strong>Following</strong> 135</div>
+                    <div class="pr-3"><strong>Followers</strong> 80M</div>
+                </div>
+
+
+        </div>
+    </div>
+
+
+</div>
+@endsection
